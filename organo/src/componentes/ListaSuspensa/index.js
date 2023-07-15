e3b168553fbdb489426a94056dba64b73425c5ef
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-07-15 12:36:36 -0300

    01-React desenvolvendo com JavaScript Finalizado

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -1,10 +1,11 @@
 import './ListaSuspensa.css';
 
 const ListaSuspensa = (props) => {
-    return(
+    return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select  onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+                <option value="">Escolha um time</option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
