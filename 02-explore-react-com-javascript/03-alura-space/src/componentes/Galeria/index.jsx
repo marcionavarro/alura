2f3d80b4183436e2b5dbb284bb59124505ba83df
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-18 01:36:35 -0300

    03-alura-space - Finalizado

--- 02-explore-react-com-javascript/03-alura-space/src/componentes/Galeria/index.jsx
+++ 02-explore-react-com-javascript/03-alura-space/src/componentes/Galeria/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tags from '../Tags'
 import styles from './Galeria.module.scss';
 import fotos from './fotos.json';
@@ -6,11 +6,21 @@ import fotos from './fotos.json';
 import Cards from './Cards';
 
 export default function Galeria() {
+    const [itens, setItens] = useState(fotos);
+    const tags = [...new Set(fotos.map((valor) => valor.tag))];
+   
+    const filtraFotos = (tag) => {
+        const novasFotos = fotos.filter((foto) => {
+            return foto.tag === tag;
+        });
+        setItens(novasFotos);
+    }
+
     return (
         <section className={styles.galeria}>
             <h2>Navegue pela galeria</h2>
-            <Tags />
-            <Cards itens={fotos} styles={styles} />
+            <Tags tags={tags} filtraFotos={filtraFotos} setItens={setItens}/>
+            <Cards itens={itens} styles={styles} />
         </section>
     )
 }
