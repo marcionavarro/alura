1a9171822502199f54b21ff7f3826101e3f32305
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-01-30 23:48:38 -0300

    05-ola-mundo - Add Paginas

---
+++ 02-explore-react-com-javascript/05-ola-mundo/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css';
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
